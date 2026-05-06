5ba5fadc6362b6b8fa2f95a50eeba863b77f407b
Anthony Perez <aperez327@gmail.com>
2026-05-06 16:34:57 -0400

    Two Button Challenge

--- YouAreAwesome/ContentView.swift
+++ YouAreAwesome/ContentView.swift
@@ -8,21 +8,44 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State var message = "I am a great developer!"
+    @State private var message = ""
+    @State private var imageString = ""
     var body: some View {
         
         VStack {
-            Image(systemName: "swift")
+            Spacer()
+            
+            Image(systemName: imageString)
                 .resizable()
                 .scaledToFit()
                 .foregroundStyle(.orange)
+                
             Text(message)
                 .font(.largeTitle)
-                .fontWeight(.heavy)
-                .foregroundStyle(.red)
-            Button("Click Me!") {
-                message = "Awesome"
+                .fontWeight(.ultraLight)
+            
+            Spacer()
+            
+            
+            Button("Press Me!") {
+                let message1 = "Yoou are Awesome!"
+                let message2 = "You are Great!"
+                let imageString1 = "hand.thumbsup"
+                let imageString2 = "sun.max.fill"
+                
+//                if message == message1 {
+//                    message = message2
+//                    imageString = imageString2
+//                } else {
+//                    message = message1
+//                    imageString = imageString1
+//                }
+                message = (message == message1 ? message2 : message1)
+                imageString = (imageString == imageString1 ? imageString2 : imageString1)
             }
+            .buttonStyle(.borderedProminent)
+            .font(.title2)
+            .tint(.orange)
         }
         .padding()
         
@@ -32,3 +55,4 @@ struct ContentView: View {
 #Preview {
     ContentView()
 }
+// print(person == "Limor" ? "Hello, Lady Ada" : "Hello, There!")
