5bd29be6c6d29cfcb72dae8954110cba56586fdc
Anthony Perez <aperez327@gmail.com>
2026-05-13 15:19:36 -0400

    Sounds While Loop/ Repeat Loop

--- YouAreAwesome/ContentView.swift
+++ YouAreAwesome/ContentView.swift
@@ -6,6 +6,7 @@
 //
 
 import SwiftUI
+import AVFAudio
 
 struct ContentView: View {
     @State private var message = ""   // Sring Data Type
@@ -14,6 +15,10 @@ struct ContentView: View {
 //    @State private var messageNumber = 0
     @State private var lastMessageNumber = -1 // lastMessageNumber will never be -1
     @State private var lastImageNumber = -1
+    @State private var lastSoundNumber = -1
+    @State private var audioPlayer: AVAudioPlayer!
+    let numberOfImages = 10 // images labeled image0 - image 9
+    let numberOfSounds = 6 // sounds labeled sound0 - sound5
     
     var body: some View {
         
@@ -24,6 +29,9 @@ struct ContentView: View {
                 .fontWeight(.heavy)
                 .foregroundStyle(.red)
                 .multilineTextAlignment(.center)
+                .animation(.easeInOut(duration: 0.15), value: message)
+            
+            Spacer()
             
             Image(imageName)
                 .resizable()
@@ -32,8 +40,6 @@ struct ContentView: View {
                 .shadow(radius: 30)
                 .animation(.default, value: imageName)
                 
-            
-            
             Spacer()
             
             Button("Show Message!") {
@@ -47,20 +53,6 @@ struct ContentView: View {
                                 "Working Hard Developing!",
                                 "So Beautiful"]
                 
-                // generate a random messageNumber to use as an index
-                // if messageNumber == lastMessageNumber {
-                //    keep generating a new random messageNumber
-                //    until you get a messageNumber != lastMessageNumber
-                // set messageString to messages[messageNumber]
-                // update the lastMessageNumber with messageNumber
-                
-//                var messageNumber = Int.random(in: 0...messages.count-1)
-//                while messageNumber == lastMessageNumber {
-//                    messageNumber = Int.random(in: 0...messages.count-1)
-//                }
-//                message = messages[messageNumber]
-//                lastMessageNumber = messageNumber
-                
                 var messageNumber: Int
                 repeat {
                     messageNumber = Int.random(in: 0...messages.count-1)
@@ -68,14 +60,31 @@ struct ContentView: View {
                 message = messages[messageNumber]
                 lastMessageNumber = messageNumber 
 
-                
                 // TODO: -Update the imageName variable-
-                var imageNumber = Int.random(in: 0...9)
-                while imageNumber == lastImageNumber {
-                    imageNumber = Int.random(in: 0...9)
-                }
+                var imageNumber: Int
+                repeat {
+                    imageNumber = Int.random(in: 0...(numberOfImages-1))
+                } while imageNumber == lastImageNumber
                 imageName = "image\(imageNumber)"
                 lastImageNumber = imageNumber
+                
+                var soundNumber: Int
+                repeat {
+                    soundNumber = Int.random(in: 0...(numberOfSounds-1))
+                } while soundNumber == lastSoundNumber
+                lastSoundNumber = soundNumber
+                let soundName = "sound\(soundNumber)"
+                
+                guard let soundFile = NSDataAsset(name: soundName) else {
+                    print("😡 could not read file named \(soundName)")
+                    return
+                }
+                do {
+                    audioPlayer = try AVAudioPlayer(data: soundFile.data)
+                    audioPlayer.play()
+                } catch {
+                    print("😡 ERROr: \(error.localizedDescription) creating audioPlayer")
+                }
             }
             .buttonStyle(.borderedProminent)
             .font(.title2)
